6b1759863e96809b1e677128850187d0131bd024
Kun Hwi Ko <kunko@seas.upenn.edu>
2020-07-22 18:25:37 -0400

    fix main function

--- server.c
+++ server.c
@@ -155,6 +155,23 @@ void* start_server(void* PORT_NUMBER)
 
 int main(int argc, char *argv[])
 {
-    int port_number = 3000; 
-    return 0;
+  int port_number = 3000; 
+
+  // generate lock and threads
+  pthread_mutex_init(&lock, NULL);
+  pthread_mutex_init(&lock2, NULL);
+  pthread_t t1;
+  if (pthread_create(&t1, NULL, &get_data, NULL)) return 1;
+  pthread_t t2;
+  if (pthread_create(&t2, NULL, &start_server, &port_number)) return 1;
+  pthread_t t3; 
+  if (pthread_create(&t3, NULL, &quit_program, NULL)) return 1;
+   
+  // join threads
+  if(pthread_join(t1,NULL) != 0) return 1;
+  if(pthread_join(t2,NULL) != 0) return 1;
+  if(pthread_join(t3,NULL) != 0) return 1;
+  
+
+  return 0;
 }
